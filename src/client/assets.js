b2c36e6890f4cf948aceee07b2c81620c39321fc
Julien Blin <julien.blin@gmail.com>
2013-06-25 19:32:54 -0400

    Added handsontable javascript.

--- src/client/assets.js
+++ src/client/assets.js
@@ -11,8 +11,15 @@ module.exports = function(assets) {
     assets.addJs('/javascripts/behaviors.js');
 
     // Project specifics
+    assets.addJs('/javascripts/numeral.js', 'project');
+    assets.addJs('/javascripts/jquery.handsontable.js', 'project');
+    assets.addJs('/javascripts/jquery.contextMenu.js', 'project');
+    assets.addJs('/javascripts/jquery.ui.position.js', 'project');
     assets.addJs('/javascripts/projectengine.js', 'project');
 
     assets.addCss('/stylesheets/bootstrap.min.css');
+    assets.addCss('/stylesheets/jquery.handsontable.css');
+    assets.addCss('/stylesheets/jquery.handsontable.bootstrap.css');
+    assets.addCss('/stylesheets/jquery.contextMenu.css');
     assets.addCss('/stylesheets/app.css');
 }
--- src/client/javascripts/projectengine.js
+++ src/client/javascripts/projectengine.js
@@ -1,11 +1,74 @@
 window.ProjectEngine = function(id) {
+    var that = this;
     this.id = id;
+    this.data = {
+        estimations: [
+            {
+                isActive: true,
+                title: 'here is the first line',
+                scale: 'Umbraco',
+                complexity: 'Medium',
+                coefficient: 1.0,
+                baseline: 25,
+                overBaseline: 20,
+                total: 6,
+                totalMoney: 3300
+            }
+        ]
+    };
+    this.gridContainers = {};
+    this.renderers = {
+        computed: function(instance, td, row, col, prop, value, cellProperties) {
+            Handsontable.NumericCell.renderer.apply(this, arguments);
+            if(!value) {
+                td.style.background = '#f2dede';
+            } else {
+                td.style.background = '#dff0d8';
+            }
+            td.style.textAlign = 'right';
+            return td;
+        }
+    };
 };
 
 ProjectEngine.prototype.init = function() {
+    var that = this;
     var loadingAlert = alerts.info('Loading project...');
-    setTimeout(function() {
-        loadingAlert.dismiss();
-        var successAlert = alerts.success('Project loaded - good to go!', 3000);
-    }, 3000);
-}
+
+    this.gridContainers.estimations = $('#estimationsGrid');
+    this.gridContainers.estimations.handsontable({
+        data: that.data.estimations,
+        colHeaders: [ "Act.", "Title", "Scale", "Complexity", "Coeff.", "Baseline", "Over-baseline", "Total", "Total $"  ],
+        colWidths:  [30,        600,     80,     80,        40,       80,         80,              80,      80],
+        stretchH: 'all',
+        rowHeaders: true,
+        minSpareRows: 1,
+        columns: [
+            { data: 'isActive',      type: 'checkbox' },
+            { data: 'title',         type: 'text' },
+            { data: 'scale',         type: 'text' },
+            { data: 'complexity',    type: 'text' },
+            { data: 'coefficient',   type: 'numeric', format: '0.0' },
+            { data: 'baseline',      type: 'numeric', format: '0 h.', readOnly: true },
+            { data: 'overBaseline', type: 'numeric', format: '0 h.', readOnly: true },
+            { data: 'total',         type: 'numeric', format: '0 d.', readOnly: true },
+            { data: 'totalMoney',   type: 'numeric', format: '0 $', readOnly: true }
+        ],
+        cells: function (row, col, prop) {
+            var cellProperties = {};
+            if(col > 4) {
+                cellProperties.renderer = that.renderers.computed;
+            };
+            return cellProperties;
+        },
+        beforeChange: function (changes) {
+            var instance = that.gridContainers.estimations.data('handsontable');
+            for (var changesIndex = 0; changesIndex < changes.length; ++changesIndex) {
+                console.log(changes[changesIndex]);
+            }
+        }
+    });
+
+    loadingAlert.dismiss();
+    alerts.success('Project loaded - good to go!', 3000);
+};
