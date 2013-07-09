07639425388ae642b8b930353f11747bfc36835c
Julien Blin <julien.blin@gmail.com>
2013-07-09 16:19:12 -0400

    Added detailed computed values to projectCalculator.

--- src/client/javascripts/projectCalculator.js
+++ src/client/javascripts/projectCalculator.js
@@ -41,6 +41,7 @@
             _.each(data.scales, function(scale) {
                 _.each(scale.lines, function(scaleLine) {
                     if(!scaleLine.computed) scaleLine.computed = {};
+
                     if(!scaleLine.values) scaleLine.values = {};
 
                     if(!scaleLine.id) {
@@ -61,12 +62,15 @@
                     scaleLine.computed.totalUT = _.reduce(scaleLine.values, function(sum, value, key) {
                         var colInfo = _getScaleColumnInfo(scale, key);
                         if(!colInfo) return sum;
+
                         var profile = _getProfile(data, colInfo.profile);
                         if(!(profile && profile.isActive)) return sum;
 
                         if(colInfo.isBaseline) return sum;
                         return sum + (totalBaseline * (self.parseFloat(value) / 100));
                     }, totalBaseline);
+
+                    scaleLine.computed.profiles = {};
 
                     scaleLine.computed.totalPrice = _.reduce(scaleLine.values, function(sum, value, key) {
                         var colInfo = _getScaleColumnInfo(scale, key);
@@ -79,8 +83,22 @@
                         if(!colInfo.isBaseline)
                             localUT = totalBaseline * (localUT / 100);
 
+                        // Details per profiles
+                        if(!scaleLine.computed.profiles[profile.id]) {
+                            scaleLine.computed.profiles[profile.id] = {};
+                            scaleLine.computed.profiles[profile.id].totalUT = 0;
+                            scaleLine.computed.profiles[profile.id].totalPrice = 0;
+                        }
+                        scaleLine.computed.profiles[profile.id].totalUT = scaleLine.computed.profiles[profile.id].totalUT + localUT;
+                        scaleLine.computed.profiles[profile.id].totalPrice = scaleLine.computed.profiles[profile.id].totalPrice + (profile.computed.priceAverage * localUT);
+
                         return sum + (profile.computed.priceAverage * localUT);
                     }, 0);
+
+                    _.each(scaleLine.computed.profiles, function(values) {
+                        values.percentUT = scaleLine.computed.totalUT == 0 ? 0 : (values.totalUT * 100) / scaleLine.computed.totalUT;
+                        values.percentPrice = scaleLine.computed.totalPrice == 0 ? 0 : (values.totalPrice * 100) / scaleLine.computed.totalPrice;
+                    });
                 });
             });
         };
@@ -115,15 +133,18 @@
 
             headingTotalLine.computed.totalUT = 0;
             headingTotalLine.computed.totalPrice = 0;
+            headingTotalLine.computed.profiles = {};
 
             var currentHeading1 = {
                 totalUT: 0,
-                totalPrice: 0
+                totalPrice: 0,
+                profiles: {}
             };
 
             var currentHeading2 = {
                 totalUT: 0,
-                totalPrice: 0
+                totalPrice: 0,
+                profiles: {}
             };
 
             for(var lineIndex = data.estimationLines.length - 1; lineIndex >= 0; --lineIndex) {
@@ -135,9 +156,16 @@
                 if(line.lineType === 'heading1') {
                     line.computed.totalUT = currentHeading1.totalUT;
                     line.computed.totalPrice = currentHeading1.totalPrice;
+                    line.computed.profiles = {};
+                    _.each(currentHeading1.profiles, function(values, profileId) {
+                        line.computed.profiles[profileId] = values;
+                        line.computed.profiles[profileId].percentUT = line.computed.totalUT == 0 ? 0 : ((line.computed.profiles[profileId].totalUT * 100) / line.computed.totalUT);
+                        line.computed.profiles[profileId].percentPrice = line.computed.totalPrice == 0 ? 0 : ((line.computed.profiles[profileId].totalPrice * 100) / line.computed.totalPrice);
+                    });
                     currentHeading1 = {
                         totalUT: 0,
-                        totalPrice: 0
+                        totalPrice: 0,
+                        profiles: {}
                     };
                     continue;
                 }
@@ -145,9 +173,16 @@
                 if(line.lineType === 'heading2') {
                     line.computed.totalUT = currentHeading2.totalUT;
                     line.computed.totalPrice = currentHeading2.totalPrice;
+                    line.computed.profiles = {};
+                    _.each(currentHeading2.profiles, function(values, profileId) {
+                        line.computed.profiles[profileId] = values;
+                        line.computed.profiles[profileId].percentUT = line.computed.totalUT == 0 ? 0 : ((line.computed.profiles[profileId].totalUT * 100) / line.computed.totalUT);
+                        line.computed.profiles[profileId].percentPrice = line.computed.totalPrice == 0 ? 0 : ((line.computed.profiles[profileId].totalPrice * 100) / line.computed.totalPrice);
+                    });
                     currentHeading2 = {
                         totalUT: 0,
-                        totalPrice: 0
+                        totalPrice: 0,
+                        profiles: {}
                     };
                     continue;
                 }
@@ -156,6 +191,7 @@
                 if(!scaleLine) {
                     line.computed.totalUT = null;
                     line.computed.totalPrice = null;
+                    line.computed.profiles = {};
                     continue;
                 }
 
@@ -178,8 +214,65 @@
                             currentHeading2.totalPrice = currentHeading2.totalPrice + line.computed.totalPrice;
                         }
                     }
+
+                    line.computed.profiles = {};
+
+                    if(line.isActive) {
+                        _.each(scaleLine.computed.profiles, function(values, profileId) {
+                            line.computed.profiles[profileId] = {};
+                            line.computed.profiles[profileId].percentUT = values.percentUT;
+                            line.computed.profiles[profileId].percentPrice = values.percentPrice;
+                            line.computed.profiles[profileId].totalUT =  line.computed.totalUT * (values.percentUT / 100);
+                            line.computed.profiles[profileId].totalPrice = line.computed.totalPrice * (values.percentPrice / 100);
+
+                            if(!headingTotalLine.computed.profiles[profileId]) {
+                                headingTotalLine.computed.profiles[profileId] = {};
+                                headingTotalLine.computed.profiles[profileId].totalUT = 0;
+                                headingTotalLine.computed.profiles[profileId].totalPrice = 0;
+                            }
+                            headingTotalLine.computed.profiles[profileId].totalUT = headingTotalLine.computed.profiles[profileId].totalUT + line.computed.profiles[profileId].totalUT;
+                            headingTotalLine.computed.profiles[profileId].totalPrice = headingTotalLine.computed.profiles[profileId].totalPrice + line.computed.profiles[profileId].totalPrice;
+
+                            if(!currentHeading1.profiles[profileId]) {
+                                currentHeading1.profiles[profileId] = {};
+                                currentHeading1.profiles[profileId].totalUT = 0;
+                                currentHeading1.profiles[profileId].totalPrice = 0;
+                            }
+                            currentHeading1.profiles[profileId].totalUT = currentHeading1.profiles[profileId].totalUT + line.computed.profiles[profileId].totalUT;
+                            currentHeading1.profiles[profileId].totalPrice = currentHeading1.profiles[profileId].totalPrice + line.computed.profiles[profileId].totalPrice;
+
+                            if(!currentHeading2.profiles[profileId]) {
+                                currentHeading2.profiles[profileId] = {};
+                                currentHeading2.profiles[profileId].totalUT = 0;
+                                currentHeading2.profiles[profileId].totalPrice = 0;
+                            }
+                            currentHeading2.profiles[profileId].totalUT = currentHeading2.profiles[profileId].totalUT + line.computed.profiles[profileId].totalUT;
+                            currentHeading2.profiles[profileId].totalPrice = currentHeading2.profiles[profileId].totalPrice + line.computed.profiles[profileId].totalPrice;
+                        });
+                    }
                 }
             }
+
+            _.each(headingTotalLine.computed.profiles, function(values, profileId) {
+                headingTotalLine.computed.profiles[profileId].percentUT = headingTotalLine.computed.totalUT == 0 ? 0 : ((headingTotalLine.computed.profiles[profileId].totalUT * 100) / headingTotalLine.computed.totalUT);
+                headingTotalLine.computed.profiles[profileId].percentPrice = headingTotalLine.computed.totalPrice == 0 ? 0 : ((headingTotalLine.computed.profiles[profileId].totalPrice * 100) / headingTotalLine.computed.totalPrice);
+            });
+
+            if(!data.computed) data.computed = {};
+            data.computed.totalUT = headingTotalLine.computed.totalUT;
+            data.computed.totalPrice = headingTotalLine.computed.totalPrice;
+            data.computed.profiles = headingTotalLine.computed.profiles;
+        };
+
+        var _profileStatistics = function(data) {
+            _.each(data.profiles, function(profile) {
+                if(!profile.computed) profile.computed = {};
+                if (!profile.id) {
+                    profile.computed.percentTotalUT = null;
+                    profile.computed.percentTotalPrice = null;
+                    return;
+                }
+            });
         };
 
         // Public functions
@@ -209,6 +302,7 @@
             _averageProfiles(data);
             _scales(data);
             _estimationLines(data);
+            _profileStatistics(data);
         };
 
         return self;
