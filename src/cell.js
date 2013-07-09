8abdbcba66bbb14efaf05e8e3adbcf144e08ffff
Julien Blin <julien.blin@gmail.com>
2013-07-08 20:42:39 -0400

    Added jslint tool.

--- src/cell.js
+++ src/cell.js
@@ -2,7 +2,10 @@
  * Main entry point for Cell node application.
  */
 
+/* jslint node: true */
+"use strict";
+
 var config = require('./config'),
     server = require('./server/server');
 
-server(config, function() {});
+server(config, function () {});
